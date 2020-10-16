a33997b5b16719a7033416565c5ef5b1be2ee847
José Eduardo <57038911+eduardomalikoski@users.noreply.github.com>
2020-10-16 19:28:05 -0300

    Exercicio5

---
+++ Exercicio.5/Pessoa.cs
@@ -0,0 +1,14 @@
+namespace Exercicio._5
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public Pessoa(string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+    }
+}
---
+++ Exercicio.5/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercicio._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa("Joao", 15);
+            Pessoa pessoa2 = new Pessoa("Leandro", 21);
+            Pessoa pessoa3 = new Pessoa("Paulo", 16);
+            Pessoa pessoa4 = new Pessoa("Jessica", 18);
+
+            List<Pessoa> pessoas = new List<Pessoa>();
+
+            pessoas.Add(pessoa1);
+            pessoas.Add(pessoa2);
+            pessoas.Add(pessoa3);
+            pessoas.Add(pessoa4);
+
+            System.Console.WriteLine("Total de pessoas:");
+            Console.WriteLine(pessoas.Count);
+
+            if (pessoa1.Idade < 18)
+            {
+                pessoas.Remove(pessoa1);
+            }
+                
+
+            if (pessoa2.Idade < 18)
+            {
+                pessoas.Remove(pessoa2);
+            }
+                
+            
+            if (pessoa3.Idade < 18)
+            {
+                pessoas.Remove(pessoa3);
+            }
+                
+
+            if (pessoa4.Idade < 18)
+            {
+                pessoas.Remove(pessoa4);
+            }
+
+            System.Console.WriteLine("Total de pessoas com menos de 18 anos:");
+            Console.WriteLine(pessoas.Count);
+
+
+        
+        }
+    
+    }  
+}
